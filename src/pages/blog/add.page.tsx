9c4d09f30f195d44e5fab87132989a730810fae2
Mohamed Abdelrahman <m7md0a.work@gmail.com>
2023-12-14 00:36:53 +0200

    update add blog page

--- src/pages/blog/add.page.tsx
+++ src/pages/blog/add.page.tsx
@@ -1,22 +1,39 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import MarkdownEditor from '@uiw/react-markdown-editor';
 import Input from "../../components/form/Input";
 
 function AddBlog() {
-  const [text, setText] = useState<string>()    
-  const [title, setTitle] = useState<string>('')    
+  const [text, setText] = useState<string>('')    
+  const [title, setTitle] = useState<string>('')
+  const [imagePreview, setImagePreview] = useState<string | ArrayBuffer | null>(null);
+
+  const handleFileChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const file = e.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setImagePreview(reader.result);
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+  }; 
   return (
-    <div className="min-h-screen space-y-5">
-        <div className="flex flex-col w-96 space-y-4 ">
-          <div className="card bg-primary w-96 h-96">Publish</div>
-          <input type="file" className="file-input file-input-primary file-input-bordered w-full" />
+    <div className="min-h-screen max-w-5xl mx-auto space-y-5">
+        <div className="flex flex-col md:flex-row gap-5">
+          <div className={`card bg-primary md:w-52 overflow-hidden rounded  ${typeof imagePreview != "string" ? "h-full" : 'max-h-full'}`}>
+            {typeof imagePreview === "string" && <img src={imagePreview} alt="Preview" className="w-full max-h-64" />}
+          </div>
+          <div className="flex flex-col w-full justify-end gap-4">
+            <h1 className="text-2xl">Title: <span className="font-bold text-primary">{title}</span></h1>
+            <div className="flex flex-col gap-4">
+              <Input auth setData={setTitle} type="text" className="w-full" placeholder="Blog title"/>
+              <input onChange={handleFileChange} type="file" className="file-input file-input-primary file-input-bordered w-full" />
+              <button className="btn btn-primary">Publish</button> 
+            </div>
+          </div>
         </div>
-        <div className="flex space-x-4">
-          <Input setData={setTitle} type="text" className="w-full" placeholder="Blog title"/>
-          <button className="btn btn-primary">Publish</button>
-        </div>
-        {title}
-        {/* <Input setData={setTitle} type="file" placeholder="dkm"/> */}
         <MarkdownEditor
             value={text}
             onChange={(value, viewUpdate) => {
